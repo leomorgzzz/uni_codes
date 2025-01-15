a166588c6cc097afe615a1e69821fb6a72377510
leomorgzzz <leomoralesrgz@gmail.com>
2025-01-14 19:30:47 -0700

    uni 2

---
+++ escuela2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int size;
+    cout << "Tamalo del arreglo: ";
+    cin >> size;
+
+    int* arr = new int [size];
+
+    for (int i = 0; i < size; i++){
+        arr[i] = i +1;
+    }
+
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << endl;
+    }
+    
+    delete[] arr;
+    
+
+
+
+    return 0; 
+}
---
+++ escuela3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int filas, columnas;
+
+    cout << "Filas: ";
+    cin >> filas;
+
+    cout << "Columnas: ";
+    cin >> columnas;
+
+    int** arr = new int*[filas];
+    for (int i = 0; i < filas; i++)
+    {
+        arr[i] = new int[columnas];
+    }
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            arr[i][j] = i +1;
+        }
+        
+    }
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            cout << arr[i][j] << " ";  
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < filas; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+    
+    return 0;
+}
